a8be1a30c3402b6164be1a608ec6e2659c832d8e
kubaadamcik <jakubadamcik11@gmail.com>
2025-11-05 10:28:37 +0100

    Added assets

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,10 +1,14 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from "./Pages/Home.tsx";
 import Courses from "./Pages/Courses.tsx";
+import NavBar from "./Components/NavBar.tsx";
 
 export default function App() {
-    return <div className="w-full h-full flex">
+    return <div className="w-full h-full flex flex-col">
         <Router>
+            <div className="flex w-full">
+                <NavBar/>
+            </div>
             <Routes>
                 <Route path="/" element={<Home/>}></Route>
                 <Route path="/courses" element={<Courses/>}></Route>
--- src/client/src/Components/NavBar.tsx
+++ src/client/src/Components/NavBar.tsx
@@ -1,5 +1,13 @@
 function NavBar() {
-    return <div className="w">Toto je navbar</div>
+    return <div className="w-full bg-blue-300 p-3">
+        <div>
+            <img className="" src="/Logo/SVG/Think-different-Academy_LOGO_oficialni-bile.svg"/>
+        </div>
+
+        <div>
+
+        </div>
+    </div>;
 }
 
 export default NavBar;
