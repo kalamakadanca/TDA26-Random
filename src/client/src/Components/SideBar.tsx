270bf5dfb1a18ee7d9142a80b9c7b606fc739422
kubaadamcik <jakubadamcik11@gmail.com>
2025-11-18 09:27:41 +0100

    added categories

--- src/client/src/Components/SideBar.tsx
+++ src/client/src/Components/SideBar.tsx
@@ -1,15 +1,19 @@
 import SideBarItem from "./SideBarItem.tsx";
 
 export default function SideBar() {
-    return <div className="w-52 h-full bg-blue-50 border-r shadow border-blue-200">
-        <div className="pb-3 pt-3 mr-3 ml-3 border-b border-gray-300">
-            <h2 className="text-2xl">Kategorie</h2>
-
-        </div>
-        <ul>
-            <li>
-                <SideBarItem></SideBarItem>
-            </li>
-        </ul>
-    </div>;
-};
+  return (
+    <div className="w-52 h-full bg-blue-50 border-r shadow border-blue-200">
+      <div className="pb-3 pt-3 mr-3 ml-3 border-b border-gray-300">
+        <h2 className="text-2xl">Kategorie</h2>
+      </div>
+      <ul>
+        <li>
+          <SideBarItem title="Programování"></SideBarItem>
+          <SideBarItem title="Kyberbezpečnost"></SideBarItem>
+          <SideBarItem title="Počítačové sítě"></SideBarItem>
+          <SideBarItem title="Informatika"></SideBarItem>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/client/src/Components/SideBarItem.tsx
+++ src/client/src/Components/SideBarItem.tsx
@@ -1,7 +1,11 @@
-export default function SideBarItem()
-{
-    
-    return <div className="w-full h-full flex p-3 items-center hover:bg-blue-200 rounded cursor-pointer">
-        <h3>Programování</h3>
+interface Props {
+  title: string;
+}
+
+export default function SideBarItem({title}: Props) {
+  return (
+    <div className="w-full h-full flex p-3 items-center hover:bg-blue-200 rounded cursor-pointer">
+      <h3>{title}</h3>
     </div>
+  );
 }
