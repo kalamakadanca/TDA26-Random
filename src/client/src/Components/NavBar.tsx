107ab4355776af92419060a048af0e65e3e35cfc
kalamakadanca <kalamakadanca@seznam.cz>
2025-12-14 22:46:46 +0100

    Refactor NavBar and Home components for improved readability and consistency; remove unused database files.

--- src/client/src/Components/NavBar.tsx
+++ src/client/src/Components/NavBar.tsx
@@ -1,65 +1,85 @@
-import {useNavigate, useLocation,} from 'react-router-dom';
-import {useEffect, useState} from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import SearchBar from "./SearchBar.tsx";
-import {me} from "../Scripts/authHelper.ts"
+import { me } from "../Scripts/authHelper.ts";
 import AccountDropdown from "./AccountDropdown.tsx";
 
 function NavBar() {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const navigateHome = () => {
-        navigate("/");
+  const navigate = useNavigate();
+  const location = useLocation();
+  const navigateHome = () => {
+    navigate("/");
+  };
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [showUserMenu, setShowUserMenu] = useState<boolean>(false);
+
+  useEffect(() => {
+    const check = async () => {
+      setIsAuthenticated(await me());
     };
 
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [showUserMenu, setShowUserMenu] = useState<boolean>(false);
+    check();
+  }, []);
 
-    useEffect(() => {
-        const check = async () => {
-            setIsAuthenticated(await me())
-        };
+  const logout = async () => {
+    const res = await axios.post("http://localhost:5196/api/auth/logout");
 
-        check();
-    }, []);
+    if (res.status === 200) {
+      window.location.reload();
+    }
+  };
 
-    const logout = async () => {
-        const res = await axios.post("http://localhost:5196/api/auth/logout");
+  return (
+    <div
+      className={
+        location.pathname == "/login" || location.pathname == "/register"
+          ? "hidden"
+          : "w-full bg-[#0070BB] p-3 flex justify-between shadow items-center flex-1"
+      }
+    >
+      <div onClick={navigateHome} className="cursor-pointer">
+        <img
+          className="w-64"
+          src="/Logo/SVG/Think-different-Academy_LOGO_oficialni-bile.svg"
+          alt="Logo Tour de App"
+        />
+      </div>
 
-        if (res.status === 200) {
-            window.location.reload();
-        }
-    };
+      <div className={location.pathname == "/courses" ? "" : "hidden"}>
+        <SearchBar />
+      </div>
 
-    return <div
-        className={(location.pathname == "/login" || location.pathname == "/register") ? "hidden" : "w-full bg-blue-500 p-3 flex justify-between shadow items-center flex-1"}>
-        <div onClick={navigateHome} className="cursor-pointer">
-            <img className="w-64" src="/Logo/SVG/Think-different-Academy_LOGO_oficialni-bile.svg"
-                 alt="Logo Tour de App"/>
-        </div>
-
-        <div className={location.pathname == "/courses" ? "" : "hidden"}>
-            <SearchBar/>
-        </div>
-
-        <div className="">
-            {isAuthenticated ?
-                <div className="w-full h-full flex flex-row gap-5 justify-center items-center">
-                    <div className="relative"
-                         onMouseEnter={() => setShowUserMenu(true)}
-                         onMouseLeave={() => setShowUserMenu(false)}>
-                        <div className="bg-blue-50 p-1 relative rounded-full cursor-pointer">
-                            <img src='/user-icon.png' className=" size-10" alt="Ikona uživatele"/>
-
-                        </div>
-                        {showUserMenu && <AccountDropdown onLogout={logout}/>}
-                    </div>
-                </div>
-                :
-                <button className="p-3 bg-blue-50 rounded" onClick={() => navigate("/login")}>Přihlásit se</button>
-            }
-        </div>
-    </div>;
+      <div className="">
+        {isAuthenticated ? (
+          <div className="w-full h-full flex flex-row gap-5 justify-center items-center">
+            <div
+              className="relative"
+              onMouseEnter={() => setShowUserMenu(true)}
+              onMouseLeave={() => setShowUserMenu(false)}
+            >
+              <div className="bg-blue-50 p-1 relative rounded-full cursor-pointer">
+                <img
+                  src="/user-icon.png"
+                  className=" size-10"
+                  alt="Ikona uživatele"
+                />
+              </div>
+              {showUserMenu && <AccountDropdown onLogout={logout} />}
+            </div>
+          </div>
+        ) : (
+          <button
+            className="p-3 bg-[#49B3B4] rounded"
+            onClick={() => navigate("/login")}
+          >
+            Přihlásit se
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
--- src/client/src/Pages/Home.tsx
+++ src/client/src/Pages/Home.tsx
@@ -1,17 +1,28 @@
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-    const navigate = useNavigate();
-    const navigateTo = (url: string) => {
-        navigate(url);
-    }
+  const navigate = useNavigate();
+  const navigateTo = (url: string) => {
+    navigate(url);
+  };
 
-    return <div className="container h-full flex flex-col justify-center items-center p-3 gap-5">
+  return (
+    <div className="w-full bg-[#1A1A1A] h-full flex flex-col p-3 gap-5">
+      <h1 className="text-[#91F5AD] flex justify-center text-5xl font-bold mb-5">
+        Katalog kurzů
+      </h1>
+      <div className="flex flex-col justify-center items-center flex-1 gap-5">
         <h3 className="text-3xl">Hello TdA</h3>
 
-        <button className="bg-blue-300 p-3 rounded-lg" onClick={() => navigateTo("/courses")}>Přejít na kurzy</button>
-
+        <button
+          className="bg-blue-300 p-3 rounded-lg"
+          onClick={() => navigateTo("/courses")}
+        >
+          Přejít na kurzy
+        </button>
+      </div>
     </div>
+  );
 }
 
 export default Home;
