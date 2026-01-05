5458d8a13cdf5dc14751052224c087a53af6a021
Your Name <89549464+elex1c@users.noreply.github.com>
2026-01-05 09:04:23 +0100

    npm run build errors fixed

--- src/client/src/Components/CourseComponents/EditSideBar.tsx
+++ src/client/src/Components/CourseComponents/EditSideBar.tsx
@@ -8,7 +8,7 @@ interface Props {
     onDeleteModule: (moduleId: string) => void;
 }
 
-export default function EditSideBar({modules, selectedModuleId, onChangeModule, onDeleteModule, onCreateModule}: Props) {
+export default function EditSideBar({modules, selectedModuleId, onChangeModule, onDeleteModule: _onDeleteModule, onCreateModule}: Props) {
     return <div className="w-52 h-full bg-blue-50 border-r shadow border-blue-200">
         <div className="pb-3 pt-3 mr-3 ml-3 border-b border-gray-300">
             <h2 className="text-2xl">Moduly</h2>
